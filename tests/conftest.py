c86656eb4176b019a481efcf3629cd794c21ddd1
Daniel <daniel.palacz@gmail.com>
2022-03-29 20:45:16 +0200

    extend conftest.py: new_email, get_timestamp fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -14,3 +16,19 @@ def driver():
     yield driver
 
     driver.quit()
+
+
+@pytest.fixture()
+def new_email(get_timestamp):
+    numerical_timestamp = int(get_timestamp)
+    return f"test+{hex(numerical_timestamp)}@gmail.com"
+
+
+@pytest.fixture()
+def get_timestamp() -> str:
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    time = now.strftime("%H%M%S")
+    return year + month + day + time
--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,21 +1,30 @@
+import pytest
+from pytest_lazyfixture import lazy_fixture
 from src.pom.columbus.registration import RegistryPage, ConfirmationPage
 
 
-class TestRegistryForm:
-
-    # def test_user_registered(self, driver, name, surname, phone, email):
-    def test_user_registered(self, driver):
+class TestRegistryFormSanity:
+    @pytest.mark.parametrize("name, surname, phone, email",
+                             [
+                                 ("Jan", "Kowalski", "555393579", lazy_fixture("new_email"))
+                             ],
+                             )
+    def test_user_registered(self, driver, name, surname, phone, email):
         registry_page = RegistryPage(driver)
         registry_page.fill_form(
-            name="x1",
-            surname="x1",
-            phone="555393579",
-            email="x9@com",
+            name=name,
+            surname=surname,
+            phone=phone,
+            email=email,
         )
         assert registry_page.url == ConfirmationPage.URL
         confirmation_page = ConfirmationPage(driver)
         confirmation_msg = confirmation_page.get_confirmation_message()
         assert confirmation_msg == ConfirmationPage.MESSAGE
 
-    def test_did_not_user_register_lack_of_data(self, driver):
+    @pytest.mark.parametrize("name", ["Jan", ""])
+    @pytest.mark.parametrize("surname", ["Kowalski", ""])
+    @pytest.mark.parametrize("phone", ["544582596", ""])
+    @pytest.mark.parametrize("email", [lazy_fixture("new_email"), ""])
+    def test_did_not_user_register_lack_of_data(self, driver, name, surname, phone, email):
         pass
