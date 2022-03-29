55d2a869af5d63c22218ce16419f82db6f94a758
Daniel <daniel.palacz@gmail.com>
2022-03-29 18:05:17 +0200

    fix typing issues

--- src/pom/common.py
+++ src/pom/common.py
@@ -1,20 +1,22 @@
 from typing import Tuple, List
 
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class BasePage:
 
-    def __init__(self, driver: webdriver):
+    def __init__(self, driver: WebDriver):
         self.driver = driver
 
     def click(self, locator: Tuple[str, str]) -> None:
         self.driver.find_element(*locator).click()
 
-    def find_element(self, locator: Tuple[str, str]) -> webdriver:
+    def find_element(self, locator: Tuple[str, str]) -> WebElement:
         return self.driver.find_element(*locator)
 
-    def find_elements(self, locator: Tuple[str, str]) -> List[webdriver]:
+    def find_elements(self, locator: Tuple[str, str]) -> List[WebElement]:
         return self.driver.find_elements(*locator)
 
     def open(self, url: str) -> None:
